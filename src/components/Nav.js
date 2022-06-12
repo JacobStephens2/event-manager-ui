aac9c19e9ee26f1760c8d65745d127022665cb01
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-12 10:02:48 -0400

    move nav styles to css file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,10 @@
+import './Nav.css';
 import React from 'react';
 import { Link } from "react-router-dom";
 
 const Login = () => (
-  <nav
-    style={{
-      borderBottom: "solid 1px",
-      paddingBottom: "1rem",
-      paddingTop: "0.7rem",
-    }}
-  >
-    <Link to="/">Home</Link>&emsp;
+  <nav>
+    <Link to="/">Event Manager</Link>&emsp;
     <Link to="/sign-up">Sign Up</Link>&emsp;
     <Link to="/login">Log In</Link>
   </nav>
