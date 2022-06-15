b178ab08f9f6d985595e127bb7e987462baaafe0
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-15 19:53:23 -0400

    fetch from api

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,26 @@
 import './Home.css';
 import Nav from '../components/Nav.js';
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+
+  const [data, setData] = useState({ text: 'hi' });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('http://api.eventmanager.stewardgoods.local/')
+      const body = result.text();
+      setData(body);
+    }
+    fetchData();
+    setData({ text: 'hello' });
+  }, []);
+
   return (
     <>
       <Nav />
       <h1>Event Manager</h1>
+      <p>The following text is from the API: {data.text}</p>
     </>
   );
 }
