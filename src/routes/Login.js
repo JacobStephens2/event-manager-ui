bfeb5c3c2c74a5786ec00b42a14a67c8ee22e136
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-17 18:26:21 -0400

    change login form to Formik form

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,50 +1,95 @@
 import React from 'react';
 import Nav from '../components/Nav.js';
 import './Login.css';
+import { useFormik } from 'formik';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less';
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less';
   }
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
   }
 
-  render() {
-    return (
-      <>
-        <Nav />
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <label htmlFor='email'>
-            Email
-            <input type='email' id='email' />
-          </label>
-          <label htmlFor='password'>
-            Password
-            <input type='password' id='password' />
-          </label>
-          <label htmlFor='submit'>
-            Login
-            <input type='submit' id='submit' />
-          </label>
-        </form>
-      </>
-    )
-  }
-}
+  return errors;
+};
+
+const Login = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <>
+      <Nav />
+      <h1>Login</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor='firstName'>First Name</label>
+        <input
+          id='firstName'
+          name='firstName'
+          type='text'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
+        />
+        {formik.touched.firstName && formik.errors.firstName ? (
+          <div>{formik.errors.firstName}</div>
+        ) : null}
+
+        <label htmlFor='lastName'>Last Name</label>
+        <input
+          id='lastName'
+          name='lastName'
+          type='text'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+        />
+        {formik.touched.lastName && formik.errors.lastName ? (
+          <div>{formik.errors.lastName}</div>
+        ) : null}
+
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  );
+};
 
 export default Login;
