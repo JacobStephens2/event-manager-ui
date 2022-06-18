be061735ac6cee01d97e81aa69c3d509fb8f779e
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-17 23:07:01 -0400

    get login page to show json response

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -10,36 +10,35 @@ const Login = () => {
       <Nav />
       <h1>Login</h1>
       <Formik
-        initialValues={{ firstName: '', lastName: '', email: '' }}
+        initialValues={{ email: '', name: '' }}
         validationSchema={Yup.object({
-          firstName: Yup.string()
-            .max(15, 'Must be 15 characters or less')
+          name: Yup.string()
             .required('Required'),
-          lastName: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required('Required'),
-          email: Yup.string().email('Invalid email address').required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required')
         })}
         onSubmit={(values, { setSubmitting }) => {
-          alert(JSON.stringify(values, null, 2));
           setSubmitting(false);
-          fetch(process.env.REACT_APP_API_ORIGIN + '/')
+          fetch(process.env.REACT_APP_API_ORIGIN + '/mimic-json', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values, null, 2)
+          })
             .then(response => response.json())
             .then(data => console.log(data));
         }}
       >
         <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" type="text" />
-          <ErrorMessage name="firstName" />
-
-          <label htmlFor="firstName">Last Name</label>
-          <Field name="lastName" type="text" />
-          <ErrorMessage name="lastName" />
-
           <label htmlFor="email">Email Address</label>
           <Field name="email" type="email" />
           <ErrorMessage name="email" />
+
+          <label htmlFor="name">Name</label>
+          <Field name="name" type="name" />
+          <ErrorMessage name="name" />
 
           <button type="submit">Login</button>
         </Form>
