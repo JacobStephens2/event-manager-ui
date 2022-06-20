9beffcdabaf2f08c8ade0cf22399d0af9c4b3f7a
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-20 17:17:20 -0400

    add request to see all events on account page

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 function Account() {
 
+  fetch(process.env.REACT_APP_API_ORIGIN + '/all_events', {
+    method: 'GET',
+    credentials: 'include'
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+
+
   return (
     <>
       <h1>Account</h1>
