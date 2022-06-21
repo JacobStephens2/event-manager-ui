1b5de7fa0d17aeb1ba8b6353ac114bb81264cafe
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-21 18:42:56 -0400

    show private list of events on account page

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import './Account.css';
 
 function Account() {
 
-  fetch(process.env.REACT_APP_API_ORIGIN + '/all_events', {
-    method: 'GET',
-    credentials: 'include'
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      fetch(process.env.REACT_APP_API_ORIGIN + '/all-events', {
+        method: 'GET',
+        credentials: 'include'
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setEvents(data);
+        });
+    };
+    fetchEvents();
+  }, [])
 
 
   return (
     <>
       <h1>Account</h1>
+      <h2>Events</h2>
+      <ul>
+        {events.map((event) =>
+          <li key={event.id}>
+            {event.name}
+          </li>
+        )}
+      </ul>
     </>
   );
 }
