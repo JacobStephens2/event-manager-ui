38538068efcf2dd695551e8d2e59203a203ba662
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-26 16:41:55 -0400

    load client list in create event form

--- src/views/Events/CreateEvent.js
+++ src/views/Events/CreateEvent.js
@@ -1,44 +1,68 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import React, { useState, useEffect } from 'react';
 
 function CreateEvent() {
 
-  const [message, setMessage] = useState([]);
+  const [message, setMessage] = useState('');
+  const [clients, setClients] = useState([]);
+  const [eventName, setEventName] = useState('');
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      fetch(process.env.REACT_APP_API_ORIGIN + '/clients', {
+        method: 'GET',
+        credentials: 'include'
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setClients(data);
+        });
+    };
+    fetchClients();
+  }, [])
+
+  function handleEventNameChange(event) {
+    setEventName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    alert('Event ' + eventName + ' was created');
+    event.preventDefault();
+    // fetch(process.env.REACT_APP_API_ORIGIN + '/event', {
+    //   method: 'POST',
+    //   credentials: 'include',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify('hello', null, 2)
+    // })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     setMessage(data.name + ' created.');
+    //   })
+  }
 
   return (
     <>
       <h1>Create Event</h1>
-      <Formik
-        initialValues={{ name: '' }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .required('Required')
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setSubmitting(false);
-          fetch(process.env.REACT_APP_API_ORIGIN + '/event', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(values, null, 2)
-          })
-            .then(response => response.json())
-            .then(data => {
-              setMessage(data.name + ' created.');
-            })
-        }}
-      >
-        <Form>
-          <label htmlFor="name">Event Name</label>
-          <Field name="name" type="text" />
-          <ErrorMessage name="name" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Event Name
+          <input name="name" type="text" value={eventName} onChange={handleEventNameChange}></input>
+        </label>
 
-          <button type="submit">Create</button>
-        </Form>
-      </Formik>
+        <label htmlFor="client">Client Name</label>
+        <select>
+          {clients.map((client) =>
+            <option key={client.id} value={client.id}>
+              {client.name}
+            </option>
+          )}
+        </select>
+
+        <input type="submit" value="Create" />
+      </form>
+
 
       <p>{message}</p>
 
