07fefe5759035d81655f6aee510d8ebf1c8c09d5
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-07-01 23:05:52 -0400

    add summary on home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,22 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 function Home() {
 
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(process.env.REACT_APP_API_ORIGIN + '/')
-        .then(response => response.json());
-      setData(result);
-    };
-    fetchData();
-  }, []);
-
   return (
     <>
       <h1>Event Manager</h1>
-      <p>The following text is from the Event Manager API:</p>
-      <p>{data.message}</p>
+      <p>This app can be used to organize event information</p>
     </>
   );
 }
